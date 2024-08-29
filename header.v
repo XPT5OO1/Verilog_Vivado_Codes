f0f6480b519a7c7fb7dd6d3fee5d4ce5cf82797b
Pallav Purbia <96231690+XPT5OO1@users.noreply.github.com>
2024-08-29 11:38:56 +0530

    Add files via upload

---
+++ header.v
@@ -0,0 +1,23 @@
+// -------------------------------------------------------------------------------
+// Copyright (c) 2024 NIT Delhi
+// Create Date: 20.08.2024 09:57:31
+// -------------------------------------------------------------------------------
+// FILE NAME: traffic_light.v
+// TYPE: module
+// PROJECT NAME: 
+// TARGET DEVICES:
+// DESCRIPTION: 
+// 
+// DEPENDENCIES: 
+// DEPARTMENT: Electrical and Electronics Engineering
+// AUTHOR'S EMAIL: pallavpurbia@gmail.com
+// -------------------------------------------------------------------------------
+// Release history
+// VERSION DATE AUTHOR DESCRIPTION
+// 1.0 20/08/2024 Pallav Initial version
+// -------------------------------------------------------------------------------
+// KEYWORDS: 
+// -------------------------------------------------------------------------------
+// PURPOSE: 
+// ADDITIONAL COMMENTS:
+// -------------------------------------------------------------------------------
